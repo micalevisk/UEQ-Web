454a33a640d50ac8350b8cd87a963ed2a7d5eb51
Micael Levi <mllc@icomp.ufam.edu.br>
2018-04-07 22:06:51 +0100

    internacionalizado e tabela gerada pelo JS

---
+++ items_PT-BR.js
@@ -0,0 +1,30 @@
+let questionnarieItems = `[
+  { "left": "irritante", "right": "divertido" },
+  { "left": "incompreensivel", "right": "compreensível" },
+  { "left": "criativo", "right": "entediante" },
+  { "left": "fácil de aprender", "right": "difícil de aprender" },
+  { "left": "valioso", "right": "insignificante" },
+  { "left": "chato", "right": "emocionante" },
+  { "left": "desinteressante", "right": "interessante" },
+  { "left": "imprevisível", "right": "previsível" },
+  { "left": "rápido", "right": "lento" },
+  { "left": "inovador", "right": "convencional" },
+  { "left": "obstrutivo", "right": "facilitador" },
+  { "left": "bom", "right": "mau" },
+  { "left": "complicado", "right": "fácil" },
+  { "left": "detestável", "right": "agradável" },
+  { "left": "comum", "right": "vanguardista" },
+  { "left": "desconfortável", "right": "confortável" },
+  { "left": "seguro", "right": "inseguro" },
+  { "left": "motivante", "right": "desmotivante" },
+  { "left": "atende as expectativas", "right": "não atende as expectativas" },
+  { "left": "ineficiente", "right": "eficiente" },
+  { "left": "claro", "right": "confuso" },
+  { "left": "não prático", "right": "prático" },
+  { "left": "organizado", "right": "desorganizado" },
+  { "left": "atraente", "right": "repulsivo" },
+  { "left": "amigável", "right": "não amigável" },
+  { "left": "conservador", "right": "inovador" }
+]`;
+
+questionnarieItems = JSON.parse(questionnarieItems);
--- script.js
+++ script.js
@@ -14,19 +14,47 @@ function tableDataToString(t, allCheckedRadios) {
   return arrAllData.join('\r\n');
 }
 
-function baixarForm() {
-  const TABLE_FORM = document.getElementById('table-form');
-  const QTD_LINHAS_UTEIS = TABLE_FORM.rows.length - 1;
+function downloadForm() {
   const allCheckedRadios = document.querySelectorAll('td > input[type=radio]:checked');
 
   const filenameEl = document.getElementById('filename');
   if (!filenameEl) return;
 
   const filename = filenameEl.value + '.csv';
-  const parsedText = tableDataToString(TABLE_FORM, allCheckedRadios);
+  const parsedText = '# item row, scale\r\n' + tableDataToString(TABLE_FORM, allCheckedRadios);
 
   if (filename &&
       !!filename.trim() &&
-      allCheckedRadios.length == QTD_LINHAS_UTEIS) download(filename, parsedText);
-  else alert('Algo Incompleto');
+      allCheckedRadios.length == questionnarieItems.length) download(filename, parsedText);
+  else alert('Fill all fields');
+}
+
+function replaceSpaces(str, replacer='-') {
+  return str.replace(/\s/g, replacer);
+}
+
+// building the table
+const TABLE_FORM = document.getElementById('table-form').getElementsByTagName('tbody')[0];
+
+for (let row=0; row < questionnarieItems.length; ++row) {
+
+  const leftItem = questionnarieItems[row].left;
+  const rightItem = questionnarieItems[row].right;
+  const leftItemName = replaceSpaces(leftItem);
+  const rightItemName = replaceSpaces(rightItem);
+
+  const newRow = TABLE_FORM.insertRow(TABLE_FORM.rows.length);
+  newRow.innerHTML += `
+    <td>${leftItem}</td>
+    <td><input name="${leftItemName}_${rightItemName}" value="-3" type="radio" required></td>
+    <td><input name="${leftItemName}_${rightItemName}" value="-2" type="radio" required></td>
+    <td><input name="${leftItemName}_${rightItemName}" value="-1" type="radio" required></td>
+    <td><input name="${leftItemName}_${rightItemName}" value="0" type="radio" required></td>
+    <td><input name="${leftItemName}_${rightItemName}" value="1" type="radio" required></td>
+    <td><input name="${leftItemName}_${rightItemName}" value="2" type="radio" required></td>
+    <td><input name="${leftItemName}_${rightItemName}" value="3" type="radio" required></td>
+    <td>${rightItem}</td>
+    <td>${row+1}</td>
+  `;
+
 }
